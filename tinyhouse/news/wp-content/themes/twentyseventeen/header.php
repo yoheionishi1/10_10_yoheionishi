1f2e5b0082b95128208a7016a3dc2785e307daff
yoheionishi1 <35599184+yoheionishi1@users.noreply.github.com>
2018-03-03 00:18:31 +0900

    Add files via upload

---
+++ tinyhouse/news/wp-content/themes/twentyseventeen/header.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="boostrap.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<title>ブックマークアプリ</title>
+</head>
+<body>
+<!-- Head[Start] -->
+<header>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href="bm_detail.php"></a>
+<a class="navbar-brand" href="us/us_select.php">ユーザー一覧</a>
+</div>
+</div>
+</nav>
+</header>
+
+<header>
+<div class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<a href="#" class="navbar-brand"><img src="gazo/logo.png"></a>
+</div>
+<div class="navbar-collapse collapse" id="navbar-main">
+<ul class="nav navbar-nav">
+<li><a href="select.php">書籍一覧</a></li>
+<li><a href="insert_view.php">書籍新規登録</a></li>
+<li><a href="user_select.php">ユーザー一覧</a></li>
+<li><a href="user_insert_view.php">ユーザー登録</a></li>
+<li><a href="login.php">ログイン</a></li>
+<li><a href="logout.php">ログアウト</a></li>
+</ul>
+
+</div>
+</div>
+</div>
+</header>
+<!-- Head[End] -->
